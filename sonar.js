e3cab74cc94ce01871316d2a0ef6e489447c2a88
Łukasz Szarejko <lukaszszarejko@wp.pl>
2022-05-24 22:33:31 +0200

    implemented generation of random land under water

--- sonar.js
+++ sonar.js
@@ -3,79 +3,120 @@ const L = 2 * H + 1;
 let u = new Array(L); // u(t)
 let u_next = new Array(L); // u(t)
 let u_prev = new Array(L); // u(t)
+let land_height = new Array(L);
 
 let img;
 let slider;
 const SCALE = 2;
 let A = 127;
 let actual_pos = H;
-const omega = 5;
+let omega = 5;
 let t = 0;
-const steps_per_frame = 5;
+const steps_per_frame = 1;
 const dt = 1 / 60 / steps_per_frame;
-const v = 0.1; // prędkość fazowa
+let v = 0.1; // prędkość fazowa
 const dx = 1 / L;
-const c2 = v * v * dt * dt / dx / dx;
-const alpha = 0.5;
+let c2 = v * v * dt * dt / dx / dx;
+let alpha = 0.5;
 
+function prepareSliders() {
+  sliderA = createSlider(1, 30, 5);
+  sliderA.position(windowWidth/2-60, windowHeight/2+L+20);
+  sliderA.style('width', '120px');
+  textSize(15);
+  text('Amplituda sygnalu', sliderA.x + sliderA.width + 10, sliderA.y+5);
+
+  sliderOm = createSlider(1, 13, 5);
+  sliderOm.position(windowWidth/2-60, windowHeight/2+L+40);
+  sliderOm.style('width', '120px');
+  textSize(15);
+  text('Omega', sliderOm.x + sliderOm.width + 10, sliderOm.y+5);
+
+  sliderAl = createSlider(1, 10, 5);
+  sliderAl.position(windowWidth/2-60, windowHeight/2+L+60);
+  sliderAl.style('width', '120px');
+  textSize(15);
+  text('Alpha', sliderAl.x + sliderAl.width + 10, sliderAl.y+5);
+
+  sliderV = createSlider(1, 8, 5);
+  sliderV.position(windowWidth/2-60, windowHeight/2+L+80);
+  sliderV.style('width', '120px');
+  textSize(15);
+  text('Predkosc fazowa (v)', sliderV.x + sliderV.width + 10, sliderV.y+5);
+}
+
+function drawTitle() {
+	textAlign(CENTER);
+	textSize(50);
+	textStyle(BOLD);
+	fill(0);
+	text('Sonar Model', windowWidth/2, 80);
+}
+
+function generateLand() {
+	let height = 30;
+  	for (let x = 0; x < L; x++) {
+	  	let y = random([-4, -2, -1, 0, 1, 2, 4]);
+		height += y;
+		land_height[x] = height;
+	}
+}
+
+function drawLand() {
+	for (let x = 0; x < L; x++)
+		u[x][L-land_height[x]] = 0;
+}
 
 function setup() {
-  createCanvas(windowWidth-30, windowHeight-30);
-  img = createImage(L, L);
-  background(117, 230, 218)
-  // 24 154 180 -blue grotto 189ab4
-  // 5 68 94 -navy blue 05445e
-  // 212 241 244 -baby blue d4f1f4
-  slider = createSlider(1, 30, 5);
-  slider.position(windowWidth/2-60, windowHeight/2+L+60);
-  slider.style('width', '120px');
-//   slider.style('color', '189ab4')
-  textSize(20);
-  textAlign(CENTER);
-  text('Amplituda sygnalu', slider.x + slider.width/2, slider.y - 20);
+	createCanvas(windowWidth-30, windowHeight-30);
+	img = createImage(L, L);
+	background(117, 230, 218)
+	// 24 154 180 -blue grotto 189ab4
+	// 5 68 94 -navy blue 05445e
+	// 212 241 244 -baby blue d4f1f4
+	
+	prepareSliders();
+	drawTitle();
+	generateLand();
 
-  textAlign(CENTER);
-  textSize(50);
-  textStyle(BOLD);
-  fill(0);
-  text('Sonar Model', windowWidth/2, 80);
+	for (let i = 0; i < L; ++i) {
+		u[i] = new Array(L);
+		u_next[i] = new Array(L);
+		u_prev[i] = new Array(L);
+	}
 
-  for (let i = 0; i < L; ++i) {
-    u[i] = new Array(L);
-    u_next[i] = new Array(L);
-    u_prev[i] = new Array(L);
-  }
-
-  for (let x = 0; x < L; ++x)
-    for (let y = 0; y < L; ++y) {
-      u[x][y] = 0;
-      u_next[x][y] = 0;
-      u_prev[x][y] = 0;
-    }
+	for (let x = 0; x < L; ++x)
+		for (let y = 0; y < L; ++y) {
+		u[x][y] = 0;
+		u_next[x][y] = 0;
+		u_prev[x][y] = 0;
+		}
 }
 
 function update() {
+	alpha = sliderAl.value()/10;
+	v = sliderV.value()/50;
+	c2 = v * v * dt * dt / dx / dx;
+	for (let x = 1; x < L - 1; ++x)
+		for (let y = 1; y < L - 1; ++y) {
+		u_next[x][y] = 2 * u[x][y] - u_prev[x][y];
+		u_next[x][y] += c2 * (u[x + 1][y] - 2 * u[x][y] + u[x - 1][y]);
+		u_next[x][y] += c2 * (u[x][y + 1] - 2 * u[x][y] + u[x][y - 1]);
+		u_next[x][y] -= alpha * dt * (u[x][y] - u_prev[x][y]);
+		}
 
-  for (let x = 1; x < L - 1; ++x)
-    for (let y = 1; y < L - 1; ++y) {
-      u_next[x][y] = 2 * u[x][y] - u_prev[x][y];
-      u_next[x][y] += c2 * (u[x + 1][y] - 2 * u[x][y] + u[x - 1][y]);
-      u_next[x][y] += c2 * (u[x][y + 1] - 2 * u[x][y] + u[x][y - 1]);
-      u_next[x][y] -= alpha * dt * (u[x][y] - u_prev[x][y]);
-    }
+	// brzegi
+	for (let x = 0; x < L; ++x) {
+		u_next[x][0] = u_next[x][1];
+		u_next[0][x] = u_next[1][x];
+		u_next[x][L - 1] = u_next[x][L - 2];
+		u_next[L - 1][x] = u_next[L - 2][x];
+	}
 
-  // brzegi
-  for (let x = 0; x < L; ++x) {
-    u_next[x][0] = u_next[x][1];
-    u_next[0][x] = u_next[1][x];
-    u_next[x][L - 1] = u_next[x][L - 2];
-    u_next[L - 1][x] = u_next[L - 2][x];
-  }
-
-  for (let x = 0; x < L; ++x) {
-    u_prev[x] = u[x].slice();
-    u[x] = u_next[x].slice();
-  }
+	for (let x = 0; x < L; ++x) {
+		u_prev[x] = u[x].slice();
+		u[x] = u_next[x].slice();
+	}
 }
 
 function draw() {
@@ -89,12 +130,11 @@ function draw() {
     }
 
     for (let step = 0; step < steps_per_frame; ++step) {
-        A = slider.value() * 100;
+        A = sliderA.value() * 100;
+        omega = sliderOm.value();
         u[actual_pos][0] = A * sin(omega * t);
 
-        //przeszkoda
-        for (let x = H / 2; x < 3 * H / 2; x++)
-        u[x][L-20] = 0;
+        drawLand();
 
         update()
         t += dt;
